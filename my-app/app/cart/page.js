546942328d4b0a26c86b8bac9ed3f1abffc9d17f
choisj123 <98chltnwls@gmail.com>
2024-10-29 18:23:37 +0900

    part1 복습

--- my-app/app/cart/page.js
+++ my-app/app/cart/page.js
@@ -1,5 +1,5 @@
 // 'use client' // 쓰면 client component
-                // 안쓰면 server component
+// 안쓰면 server component
 
 // server component
 // - html에 자바스크립트 기능 넣기 불가능( ex, onclick 이벤트)
@@ -12,47 +12,54 @@
 // - useState, useEffect 등 사용 가능
 // - 로딩 속도 느림 (js 필요, hydration 필요)
 
-import Link from "next/link"
+import Link from "next/link";
 //import age from "./data.js" // ./ - 현재 폴더  ../ - 상위폴더 , ./폴더명/파일명 - 하위폴더
-import {age, name} from "./data.js"
-import Contents from "./contents.js"
+import { age, name } from "./data.js";
+import Contents from "./contents.js";
 
-export default function Cart(){
-    let 장바구니 = ['Tomatoes', 'Pasta']
-    let card = ['현대', '롯데', '신한']
-    return(
-        <div>
-            <Contents/>
-            <h3 className="title">장바구니</h3>
-            {/* {age} {name} */}
-            <CartItem item={장바구니[0]} temp={card[0]}/>
-            <CartItem item={장바구니[1]}/>
-            <CartItem item={장바구니[2]}/> 
-            <Banner event={card[0]}/>
-            <Banner event={card[1]}/>
-            <Banner event={card[2]}/>
-            <RedBtn color="blue"/>
-            <Link style={{textAlign: 'center'}} href="/cart/payment">결제</Link>
-        </div>
-    )
+export default function Cart() {
+  let 장바구니 = ["Tomatoes", "Pasta", "orange"];
+  let card = ["현대", "롯데", "신한"];
+  return (
+    <div>
+      <Contents />
+      <h3 className="title">장바구니</h3>
+      {/* {age} {name} */}
+      <CartItem item={장바구니[0]} temp={card[0]} />
+      <CartItem item={장바구니[1]} temp={card[1]} />
+      <CartItem item={장바구니[2]} temp={card[2]} />
+      <Banner event={card[0]} />
+      <Banner event={card[1]} />
+      <Banner event={card[2]} />
+      <RedBtn color="blue" />
+      <Link style={{ textAlign: "center" }} href="/cart/payment">
+        결제
+      </Link>
+    </div>
+  );
 }
 
-function Banner(props){
-    return <h5>{props.event}카드 결제 행사중</h5>
+function Banner(props) {
+  return <h5>{props.event}카드 결제 행사중</h5>;
 }
 
-// 컴포넌트 component 
-function CartItem(props){
-    return (
-        <div className="cart-item">
-            <p>{props.item}</p>
-            <p>{props.temp}</p>
-            <p>1개</p>
-        </div>
-    )
+// 컴포넌트 component
+function CartItem(props) {
+  return (
+    <div className="cart-item">
+      <p>{props.item}</p>
+      <p>{props.temp}</p>
+      <p>1개</p>
+    </div>
+  );
 }
 
-function RedBtn(props){
-    return <button style={{background: props.color, width:'100px', marginLeft: '400px'}}>버튼</button>
+function RedBtn(props) {
+  return (
+    <button
+      style={{ background: props.color, width: "100px", marginLeft: "400px" }}
+    >
+      버튼
+    </button>
+  );
 }
-
--- my-app/app/list/page.js
+++ my-app/app/list/page.js
@@ -1,48 +1,55 @@
-'use client'
+"use client";
 
-import Image from "next/image"
+import Image from "next/image";
 // import img from '/public/tomato.png'
 
-import {useState} from "react"
+import { useState } from "react";
 
-export default function List(){
-    let 상품 = ['Tomatoes', 'Pasta', 'Coconut'] // array 자료형
+export default function List() {
+  let 상품 = ["Tomatoes", "Pasta", "Coconut"]; // array 자료형
 
-    // let arr = [2,3,4]
-    // let a = arr.map((a, i)=>{
-    //   // 함수 안의 코드 반복 실행 해줌
-    //   console.log(a, i) 
-    //   return 10
-    // })
-    // console.log(a)
+  // let arr = [2,3,4]
+  // let a = arr.map((a, i)=>{
+  //   // 함수 안의 코드 반복 실행 해줌
+  //   console.log(a, i)
+  //   return 10
+  // })
+  // console.log(a)
 
-    let [수량, 수량변경] = useState([0, 0, 0])
-    return(
-        <div>
-            <h3 className="title">상품 목록</h3>
-            {
-                상품.map((a, i)=>{
-                    return (
-                        <div className="food" key={i}>
-                            {/* <Image src={img} alt="토마토" className="food-img"></Image> */}
-                            <img src={`/food${i}.png`} alt="토마토" className="food-img"></img>
-                            {/* <Image src="https://placehold.co/500" width={500} height={500} className="food-img"/>  외부에서 불러올 때 */}
-                            <h4>{a} $40</h4>
-                            <button onClick={()=> {
-                                let copy1 = [...수량]
-                                copy1[i]--
-                                수량변경(copy1)
-                            }}>-</button>
-                            <span> {수량[i]} </span>
-                            <button onClick={()=>{ 
-                                let copy1 = [...수량]
-                                copy1[i]++
-                                수량변경(copy1) 
-                            }}>+</button>
-                        </div>
-                    )
-                })
-            }
-        </div>
-    )
+  let [수량, 수량변경] = useState([0, 0, 0]);
+  // state 장점 : state 변경되면 state 쓰는 html도 자동 렌더링
+  return (
+    <div>
+      <h3 className="title">상품 목록</h3>
+      {상품.map((a, i) => {
+        return (
+          <div className="food" key={i}>
+            {/* <Image src={img} alt="토마토" className="food-img"></Image> */}
+            <img src={`/food${i}.png`} alt="토마토" className="food-img"></img>
+            {/* <Image src="https://placehold.co/500" width={500} height={500} className="food-img"/>  외부에서 불러올 때 */}
+            <h4>{a} $40</h4>
+            <button
+              onClick={() => {
+                let copy1 = [...수량];
+                copy1[i]--;
+                수량변경(copy1);
+              }}
+            >
+              -
+            </button>
+            <span> {수량[i]} </span>
+            <button
+              onClick={() => {
+                let copy1 = [...수량];
+                copy1[i]++;
+                수량변경(copy1);
+              }}
+            >
+              +
+            </button>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
